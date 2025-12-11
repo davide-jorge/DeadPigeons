7caf68c0aa573766aafb540da3fb1a745a57778f
davide-jorge <finndahumane3@protonmail.com>
2025-12-11 01:05:44 +0100

    Performing scaffolding

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using efscaffold;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>((serviceProvider, opts) =>
+{
+    opts.UseNpgsql(
+        builder.Configuration.GetValue<string>("Db")
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
